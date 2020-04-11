c0bac5f906b59c3b8ecb5ccad503e53e04ba6282
Yhull Freddy Arias Hoyos <freddyariash@gmail.com>
2020-04-11 15:36:11 -0500

    Implementación Icono Web type="image/svg+xml" como ticket.svg y type="image/vnd.microsoft.icon" como favicon.icoo y

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Sistema de Ticket</title>
+        
+        <link rel="icon" type="image/svg+xml" href="ticket.svg" sizes="any">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
